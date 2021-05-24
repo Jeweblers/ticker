7fc8c632341cdae72d77406a9a1e2c9c35d9ecba
Kili Man Jaare <developer.chitoraga@gmail.com>
2021-05-24 18:43:06 +0300

    feat: init firebase console and adding base configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDEF73dgRIXncwSoz9m9yjIBOjRGAUanHw",
+    authDomain: "ticker-c75e1.firebaseapp.com",
+    projectId: "ticker-c75e1",
+    storageBucket: "ticker-c75e1.appspot.com",
+    messagingSenderId: "1036141907944",
+    appId: "1:1036141907944:web:1f6c2b2fdf2dddfea39e5d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDEF73dgRIXncwSoz9m9yjIBOjRGAUanHw",
+    authDomain: "ticker-c75e1.firebaseapp.com",
+    projectId: "ticker-c75e1",
+    storageBucket: "ticker-c75e1.appspot.com",
+    messagingSenderId: "1036141907944",
+    appId: "1:1036141907944:web:1f6c2b2fdf2dddfea39e5d"
+  }
 };
 
 /*
