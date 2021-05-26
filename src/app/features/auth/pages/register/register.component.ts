0afc99b6484f93706ea254440827e717db943cdf
Kili Man Jaare <developer.chitoraga@gmail.com>
2021-05-26 17:57:19 +0300

    feat: add registration page

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {finalize} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  public form: FormGroup = new FormGroup({})
+  public loading: boolean = false
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public submit(): void {
+    this.form.markAsDirty({onlySelf: false});
+
+    Object.keys(this.form.controls).forEach((key: string) => {
+      this.form.get(key)?.markAsDirty()
+    })
+
+    if (this.form.valid) {
+      this.loading = true
+
+      this.auth
+        .register({
+          name: this.form.controls.name.value,
+          email: this.form.controls.email.value,
+          password: this.form.controls.password.value,
+        })
+        .pipe(finalize(() => this.loading = false))
+        .subscribe(async () => {
+          await this.router.navigate(['/auth/login'])
+        }, error => {
+          // TODO: Handle validation errors from server
+          console.log(error)
+        })
+    }
+  }
+
+  public hasError(control: string) {
+    return this.form.get(control)?.dirty && this.form.get(control)?.invalid;
+  }
+
+  public getErrors(control: string): ValidationErrors | null | undefined {
+    return this.form.get(control)?.errors;
+  }
+
+  private buildForm(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    })
+  }
 }
